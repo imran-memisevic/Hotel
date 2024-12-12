487f5ed4c22da51c65f24e64df54c7cd7317a6ff
eminamumic <emimumic1@gmail.com>
2024-12-12 19:05:46 +0100

    Spajanje koda, proba

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
+class Hotel {
+  korisnici = []
+  sobe = []
+  usluge = []
+  rezervacije = []
+  statusSistema = true
+
+  //Metoda koja kreira i dodaje sobe u hotel
+  dodajSobe(brojSobe, tipSobe) {
+    let soba = new Soba(brojSobe, tipSobe)
+    this.sobe.push(soba)
+  }
+
+  //Metoda koja gasi sistem tj gasi hotel
+  gasenjeSistema() {
+    if (this.statusSistema === false) {
+      console.log('Sistem je vec ugasen')
+    }
+    this.statusSistema = false
+    console.log('Sistem je ugasen')
+  }
+}
+
+//Klasa za kreiranje noih soba(jednokrevetne, dvokrevet itd)
+class Soba {
+  constructor(broj, tip) {
+    this.broj = broj
+    this.tip = tip
+    this.dostupnost = true
+  }
+
+  oslobodi() {
+    this.dostupnost = true
+  }
+
+  rezervisi() {
+    this.dostupnost = false
+  }
+}
+
+//Klasa za kreiranje novih usluga(masaza, bazen itd)
+class Usluga {
+  constructor(tip, cijenaPoDanu) {
+    this.tip = tip
+    this.cijenaPoDanu = cijenaPoDanu
+  }
+}
 class Admin {
   ime
   constructor(ime) {
