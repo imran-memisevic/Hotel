b130f2503ad8414ae76fba24e706fa4ed4ad01a8
eminamumic <emimumic1@gmail.com>
2024-12-23 11:17:21 +0100

    Izmjene u nacinu pracenj danaBoravka

--- provjera.js
+++ provjera.js
@@ -6,37 +6,30 @@ let hotel = new Hotel({
   naziv: 'Melain',
 })
 
-hotel.dodajSobe(101, 'Jednokrevetna', 50)
-hotel.dodajSobe(102, 'Dvokrevetna', 100)
-hotel.dodajSobe(103, 'Trokrevetna', 150)
+let soba101 = hotel.dodajSobe(101, 'Jednokrevetna', 50)
+let soba102 = hotel.dodajSobe(102, 'Dvokrevetna', 100)
 
 hotel.dodajUslugu('Teretana', 10)
 hotel.dodajUslugu('Kino', 20)
 
-let soba101 = hotel.sobe.get(101)
-
 const admin = new Admin('Emina')
-admin.registracijaKorisnika(
+let emina = admin.registracijaKorisnika(
   'Emina',
   'MUmic',
   'Ž',
   'OK123',
   21,
   101,
-  'Jednolrevetna',
-  new Date(),
+  'JednoKrevetna',
   'emina',
   '123',
   soba101,
   hotel
 )
 
-const emina = hotel.korisnici.get('OK123')
 emina.naruciUslugu('Teretana', hotel)
 emina.koristiUslugu('Teretana')
 emina.naruciUslugu('Kino', hotel)
-emina.koristiUslugu('Kino')
-emina.koristiUslugu('Kino')
 emina.produziDanBoravka()
 emina.produziDanBoravka()
 admin.izdajRacun(emina)
